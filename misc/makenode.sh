6aa5d733026a5151d18d6bbb3c89afb7788baf85
Paul <prg3@ip-172-31-23-64.us-west-2.compute.internal>
2016-03-02 04:15:33 +0000

    Changed the Key so the puppet server can get at the systems internally

--- misc/makenode.sh
+++ misc/makenode.sh
@@ -2,5 +2,5 @@
 
 #This is a bit messy, using 2 different aws cli interfaces, will clean it up some other day
 
-INSTANCE=`ec2-run-instances -g sg-804af4e7 --user-data-file start_puppet.sh -t t2.micro -k Shell --region us-west-2 ami-9abea4fb| grep INSTANCE | awk '{print $2}'`
+INSTANCE=`ec2-run-instances -g sg-804af4e7 --user-data-file start_puppet.sh -t t2.micro -k PuppetServer --region us-west-2 ami-9abea4fb| grep INSTANCE | awk '{print $2}'`
 aws elb register-instances-with-load-balancer --load-balancer-name Intuit  --instances $INSTANCE
